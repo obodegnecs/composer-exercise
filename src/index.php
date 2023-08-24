af695cc5a558dd9974b4dab1e27a4bd00578da35
Csenge Dobo <csenge_dobo@epam.com>
2023-08-24 11:38:47 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,13 +1,3 @@
 <?php
-require '../vendor/autoload.php';
 
-//Sorting package with command: composer require --sort-packages symfony/dotenv
-use Symfony\Component\Dotenv\Dotenv;
-
-$dotenv = new Dotenv();
-$dotenv->load(__DIR__.'/test.env');
-
-echo "<pre>";
-dump($_ENV);
-echo "</pre>";
 
