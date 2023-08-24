815d4cd9096ce44d7b04f4d29e0d7946bc12b69f
Csenge Dobo <csenge_dobo@epam.com>
2023-08-24 11:33:26 +0200

    Print the value of

--- src/index.php
+++ src/index.php
@@ -1,2 +1,13 @@
 <?php
+require '../vendor/autoload.php';
+
+//Sorting package with command: composer require --sort-packages symfony/dotenv
+use Symfony\Component\Dotenv\Dotenv;
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/test.env');
+
+echo "<pre>";
+dump($_ENV);
+echo "</pre>";
 
